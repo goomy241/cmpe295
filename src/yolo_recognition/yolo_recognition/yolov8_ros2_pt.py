b45c119834abd8646ab2b5b905836b9ac41a8b93
goomy241 <meavecc@gmail.com>
2023-11-29 20:56:12 -0800

    apply yolo to all six cameras and display results

--- src/yolo_recognition/yolo_recognition/yolov8_ros2_pt.py
+++ src/yolo_recognition/yolo_recognition/yolov8_ros2_pt.py
@@ -23,53 +23,55 @@ class Yolov8_publisher(Node):
 
         self.declare_parameter('model_path', current_dir + '/yolov8n.pt')
         self.model_path = self.get_parameter('model_path').value
-
         self.model = YOLO(self.model_path)
 
-        self.yolov8_inference = Yolov8Inference()
+        self.channels = ['CAM_BACK', 'CAM_FRONT', 'CAM_FRONT_LEFT', 'CAM_FRONT_RIGHT', 'CAM_BACK_RIGHT', 'CAM_BACK_LEFT']
+        self.yolov8_publishers = {}  # Dictionary to store publishers for each camera
 
-        self.subscription = self.create_subscription(
-            Image,
-            'nuscenes/CAM_FRONT/image',
-            self.camera_callback,
-            10)
-        self.subscription 
+        for channel in self.channels:
+            self.create_subscription(
+                Image,
+                f'/nuscenes/{channel}/image',
+                lambda data, channel=channel: self.camera_callback(data, channel),
+                10)
 
-        self.yolov8_pub = self.create_publisher(Yolov8Inference, "/yolov8/inference", 10)
-        self.img_pub = self.create_publisher(Image, "/yolov8/result", 10)
+            # Create publishers for each camera
+            inference_topic = f'/yolov8/inference/{channel}'
+            self.yolov8_publishers[channel] = self.create_publisher(Yolov8Inference, inference_topic, 10)
 
-    def camera_callback(self, data):
+    def camera_callback(self, data, channel):
 
         img = bridge.imgmsg_to_cv2(data, "8UC3")
         results = self.model(img, device=0) # cpu=cpu, gpu=0 
 
-        self.yolov8_inference.header.frame_id = "base_link"
-        self.yolov8_inference.header.stamp = self.get_clock().now().to_msg()
+        yolov8_inference = Yolov8Inference()
+        yolov8_inference.header.frame_id = f"base_link_{channel}"
+        yolov8_inference.header.stamp = self.get_clock().now().to_msg()
         # self.yolov8_inference.header = data.header
 
 
         for r in results:
             boxes = r.boxes
             for box in boxes:
-                self.inference_result = InferenceResult()
+                inference_result = InferenceResult()
                 b = box.xyxy[0].to('cpu').detach().numpy().copy()  # get box coordinates in (top, left, bottom, right) format
                 c = box.cls
-                self.inference_result.class_name = self.model.names[int(c)]
-                self.inference_result.top = int(b[0])
-                self.inference_result.left = int(b[1])
-                self.inference_result.bottom = int(b[2])
-                self.inference_result.right = int(b[3])
-                self.yolov8_inference.yolov8_inference.append(self.inference_result)
+                inference_result.class_name = self.model.names[int(c)]
+                inference_result.top = int(b[0])
+                inference_result.left = int(b[1])
+                inference_result.bottom = int(b[2])
+                inference_result.right = int(b[3])
+                yolov8_inference.yolov8_inference.append(inference_result)
 
             #camera_subscriber.get_logger().info(f"{self.yolov8_inference}")
 
-        annotated_frame = results[0].plot()
-        img_msg = bridge.cv2_to_imgmsg(annotated_frame)
-        img_msg.header = data.header
+        # Publish inference results directly without using the inference_topic variable
+        self.yolov8_publishers[channel].publish(yolov8_inference)
 
-        self.img_pub.publish(img_msg)
-        self.yolov8_pub.publish(self.yolov8_inference)
-        self.yolov8_inference.yolov8_inference.clear()
+        # Optionally, you can also publish the annotated image
+        img_msg = bridge.cv2_to_imgmsg(results[0].plot())
+        img_msg.header = data.header
+        self.create_publisher(Image, f'/yolov8/result/{channel}', 10).publish(img_msg)
 
 def main(args=None):
     rclpy.init(args=None)
